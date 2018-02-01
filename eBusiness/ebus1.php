573e67a2912ff2ec3af00e1783b865e56dfbfc79
Brian O Sullivan <117340543@umail.ucc.ie>
2018-02-01 10:47:33 +0000

    ebus

---
+++ eBusiness/ebus1.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Select Product</title>
+        <!--jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+    </head>
+    <body>
+    <h4>Select a product</h4>
+    
+    <br/>
+    
+    <form method="POST" action="Ebus2.php">
+        
+        <label for="salesforce">
+<input type="radio" id="salesforce" name="product" onclick="disablebtnProceed()"/>
+salesforce @ $100
+        </label>
+        
+     <label for="aws">
+<input type="radio" id="aws" name="product" onclick="disablebtnProceed()"/>
+AWS @ $300
+        </label>
+        
+        <br/>
+        <br/>
+        
+        <label for="subtotal">
+            Sub total
+            <input type="text" id="subtotal" value="0.00" readonly/>
+        </label>
+        
+        <br/>
+        
+         <label for="total">
+             total
+            <input type="text" id="total" value="0.00" readonly/>
+        </label>
+        
+        <br/>
+        
+        <button type="submit" id="btnProceed" disabled>Add to shopping cart</button>
+        
+    </form>
+<button Onclick="calcSub()">Calculate Cost</button>
+<a role="button" href="Ebus1.php">Clear Choice</a>
+    </body>
+</html>
